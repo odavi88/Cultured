40b24010d6fdfac5f7c664ac5f19fefbaac8de85
Omar D <omardavidson227@gmail.com>
2023-04-27 14:22:33 -0400

    Initial Commit

---
+++ Cultured/CulturedApp.swift
@@ -0,0 +1,17 @@
+//
+//  CulturedApp.swift
+//  Cultured
+//
+//  Created by Omar Davidson II on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CulturedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
