edb8276c28c117ccf704b8718878985a6ddbcd0d
Omar D <omardavidson227@gmail.com>
2023-05-08 20:23:17 -0400

    take out section from TaskListView

--- Cultured/Views/TaskListView.swift
+++ Cultured/Views/TaskListView.swift
@@ -17,65 +17,64 @@ struct TaskListView: View {
         NavigationView {
             ScrollView {
                 ForEach(vm.districts) { district in
-                    Section(district.name) {
-                        ForEach(district.tasks) { task in
-                            HStack(alignment: .top, spacing: 15) {
-                                Image(task.taskImage)
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 100, height: 100)
-                                    .cornerRadius(10)
-                                VStack(alignment: .leading, spacing: 25) {
-                                    Text(task.description)
-                                    HStack(spacing: 20) {
-                                        
-                                        NavigationLink(destination:  LocaleSuggestionsView()) {
-                                            TaskRowView()
-                                        }
-//                                        NavigationLink {
-//                                            LocaleSuggestionsView()
-//                                        } label: {
-//                                            Text("Suggestions")
-//                                                .padding(.vertical, 15)
-//                                                .padding(.horizontal, 5)
-//                                                .foregroundColor(.black)
-//                                                .background(
-//                                                    Color.accentColor
-//                                                )
-//                                                .cornerRadius(15)
-////                                        }
-//                                        .buttonStyle(PlainButtonStyle())
-                                        
-                                        NavigationLink {
-                                            DiscoveredLocationView()
-                                        } label: {
-                                            Text("Discovered!")
-                                                .padding(.vertical, 15)
-                                                .padding(.horizontal, 5)
-                                                .foregroundColor(.black)
-                                                .background(
-                                                    Color(uiColor: .systemGray5)
-                                                )
-                                                .cornerRadius(15)
-                                        } .buttonStyle(PlainButtonStyle())
-//                                        SuggestedButtonView()
-//                                        DiscoveredButtonView()
+                    ForEach(district.tasks) { task in
+                        HStack(alignment: .top, spacing: 15) {
+                            Image(task.taskImage)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 100, height: 100)
+                                .cornerRadius(10)
+                            VStack(alignment: .leading, spacing: 25) {
+                                Text(task.description)
+                                HStack(spacing: 20) {
+                                    
+                                    NavigationLink(destination:  LocaleSuggestionsView()) {
+                                        TaskRowView()
                                     }
+                                    //                                        NavigationLink {
+                                    //                                            LocaleSuggestionsView()
+                                    //                                        } label: {
+                                    //                                            Text("Suggestions")
+                                    //                                                .padding(.vertical, 15)
+                                    //                                                .padding(.horizontal, 5)
+                                    //                                                .foregroundColor(.black)
+                                    //                                                .background(
+                                    //                                                    Color.accentColor
+                                    //                                                )
+                                    //                                                .cornerRadius(15)
+                                    ////                                        }
+                                    //                                        .buttonStyle(PlainButtonStyle())
+                                    
+                                    NavigationLink {
+                                        DiscoveredLocationView()
+                                    } label: {
+                                        Text("Discovered!")
+                                            .padding(.vertical, 15)
+                                            .padding(.horizontal, 5)
+                                            .foregroundColor(.black)
+                                            .background(
+                                                Color(uiColor: .systemGray5)
+                                            )
+                                            .cornerRadius(15)
+                                    } .buttonStyle(PlainButtonStyle())
+                                    //                                        SuggestedButtonView()
+                                    //                                        DiscoveredButtonView()
                                 }
-                                
                             }
-                            Divider()
+                            
                         }
+                        Divider()
                     }
                 }
-                .listRowSeparator(.hidden)
             }
-            .listStyle(.inset)
-            .navigationTitle("Tasks") // "Downtown Tasks etc."
+            .listRowSeparator(.hidden)
         }
-        
+        .listStyle(.inset)
+        .navigationTitle("Tasks") // "Downtown Tasks etc."
     }
+    
 }
+
 
 struct TaskListView_Previews: PreviewProvider {
     static var previews: some View {
